7853443876da177532926c5f25a40a31bf5a0a5b
Payment Yamzz Real <ryanrmdn207@gmail.com>
2025-08-22 23:16:50 +0700

    Update quotes.js

--- api/quotes.js
+++ api/quotes.js
@@ -4,10 +4,10 @@ export default function handler(req, res) {
     { q: "Kegagalan adalah kesempatan untuk memulai lagi dengan lebih bijaksana.", a: "Henry Ford" },
     { q: "Kesuksesan tidak datang kepada kamu. Kamu yang harus mencapainya.", a: "Marva Collins" },
     { q: "Bahagia bukan karena segalanya sempurna, tapi karena kamu memilih bersyukur.", a: "Anonim" },
-    { q: "Lakukan yang terbaik, hasil akan mengikuti.", a: "Anonim" }
-    { q: "Kesuksesan tak datang tiba-tiba, ia lahir dari kesabaran dan usaha yang tak terlihat", a: "google" }
-    { q: "Jadikan setiap kegagalan sebagai batu loncatan menuju keberhasilan", a: "Yamzz" }
-    { q: "Kegagalan bukan lah akhir dari perjuangan" a: "yamzz" }
+    { q: "Lakukan yang terbaik, hasil akan mengikuti.", a: "Anonim" },
+    { q: "Kesuksesan tak datang tiba-tiba, ia lahir dari kesabaran dan usaha yang tak terlihat", a: "google" },
+    { q: "Jadikan setiap kegagalan sebagai batu loncatan menuju keberhasilan", a: "Yamzz" },
+    { q: "Kegagalan bukan lah akhir dari perjuangan", a: "yamzz" }
   ];
 
   const random = quotes[Math.floor(Math.random() * quotes.length)];
